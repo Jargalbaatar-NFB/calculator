d89cad976434f3685ef7200e09f53a7b46064ce6
Jargalbaatar-NFB <jbaatar501@gmail.com>
2025-05-09 19:05:55 +0800

    fix

---
+++ src/test/java/lab15/sict/DivisionTest.java
@@ -0,0 +1,23 @@
+package lab15.sict;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DivisionTest {
+
+    @Test
+    void testDivideNormal() {
+        Division calc = new Division();
+        assertEquals(2.0, calc.divide(4.0, 2.0), 0.0001);
+    }
+
+    @Test
+    void testDivideByZero() {
+        Division calc = new Division();
+        assertThrows(IllegalArgumentException.class, () -> calc.divide(4.0, 0.0),
+            "Тэгээр хуваах нь exception өгчих байх ёстой");
+    }
+}
+    
+
