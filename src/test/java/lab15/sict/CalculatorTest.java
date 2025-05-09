4920591d1ebd6b6c3a76310d5e5d3290ff0e043a
Jargalbaatar-NFB <jbaatar501@gmail.com>
2025-05-09 18:46:11 +0800

    Vrjih vildel hiisen

--- src/test/java/lab15/sict/CalculatorTest.java
+++ src/test/java/lab15/sict/CalculatorTest.java
@@ -10,6 +10,11 @@ public class CalculatorTest {
     void testAdd() {
         assertEquals(5, calc.add(2, 3));
     }
+    @Test
+    void testMultiply() {
+    assertEquals(6, calc.multiply(2, 3));
+}
+
 
     @Test
     void testSubtract() {
